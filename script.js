894e1fcafea201d0418ba431926ea616138660fc
eaemanu027 <emanuelladejesussoares09@gmail.com>
2026-06-02 17:14:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,33 @@ function irParaHistoria() {
 // - resposta certa
 // - pontuação
 // - troca de perguntas
-
+const perguntas = [
+    {
+        pergunta: "Quem é a pessoa mais linda do mundo?",
+        opcoes: ["Eu", "Ana Letícia", "Namorada da Manu", "Todas as alternativas"],
+        correta: "Todas as alternativas"
+    },
+    {
+        pergunta: "Quem disse 'Eu te amo' primeiro?",
+        opcoes: ["Gataista", "Gatista"],
+        correta: "Gatista"
+    },
+    {
+        pergunta: "Qual foi o dia que eu fui na sua casa pela primeira vez?",
+        opcoes: ["05/01/2025", "15/01/2025", "08/02/2025", "22/02/2025", "18/03/2025", "29/03/2025"],
+        correta: "29/03/2025"
+    },
+    {
+        pergunta: "Qual é o meu cachorro favorito da Patrulha Canina?",
+        opcoes: ["Skye", "Chase", "Marshall", "Rubble", "Rocky", "Zuma"],
+        correta: "Marshall"
+    },
+    {
+        pergunta: "Qual foi o dia que voltamos a nos falar este ano?",
+        opcoes: ["05/01", "18/01", "25/01", "27/01", "31/01", "02/02"],
+        correta: "31/01"
+    }
+];
 
 // =========================
 // ❤️ (FUTURO) BOTÃO "NÃO" FUGINDO
