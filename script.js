f20889f39e011b873155b452cea1bdbe5f39128e
eaemanu027 <emanuelladejesussoares09@gmail.com>
2026-06-02 17:06:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,31 @@ if (botaoSim) {
         alert("Eu sabia ❤️");
     });
 }
+
+function soltarConfete() {
+    for (let i = 0; i < 40; i++) {
+        const confete = document.createElement("div");
+
+        confete.style.position = "fixed";
+        confete.style.width = "8px";
+        confete.style.height = "8px";
+        confete.style.backgroundColor = coresConfete();
+        confete.style.left = Math.random() * window.innerWidth + "px";
+        confete.style.top = "-10px";
+        confete.style.opacity = Math.random();
+        confete.style.zIndex = 9999;
+
+        confete.style.animation = `cair ${1 + Math.random() * 2}s linear`;
+
+        document.body.appendChild(confete);
+
+        setTimeout(() => {
+            confete.remove();
+        }, 3000);
+    }
+}
+
+function coresConfete() {
+    const cores = ["#00ff88", "#ff4d6d", "#ffd166", "#4cc9f0", "#b5179e"];
+    return cores[Math.floor(Math.random() * cores.length)];
+}
